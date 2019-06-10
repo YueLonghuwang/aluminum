52384436eca4248830f80a3a3fcf383b30b2f1ae
韩常铭 <han.changming@qq.com>
2019-06-10 17:11:11 +0800

    Initial commit

---
+++ src/main/java/com/rengu/project/aluminum/AluminumApplication.java
@@ -0,0 +1,13 @@
+package com.rengu.project.aluminum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluminumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AluminumApplication.class, args);
+    }
+
+}
