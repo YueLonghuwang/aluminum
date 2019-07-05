d8457987796c36d6dac2b05fcc1b23274283273d
JiaHao <2848009082@qq.com>
2019-07-05 08:48:58 +0800

    查看历史版本信息

--- src/main/java/com/rengu/project/aluminum/repository/ApplicationRecordRepository.java
+++ src/main/java/com/rengu/project/aluminum/repository/ApplicationRecordRepository.java
@@ -1,11 +1,13 @@
 package com.rengu.project.aluminum.repository;
 
 import com.rengu.project.aluminum.entity.ApplicationRecord;
+import com.rengu.project.aluminum.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -17,6 +19,7 @@ public interface ApplicationRecordRepository extends JpaRepository<ApplicationRe
 
     Optional<ApplicationRecord> findByProcessId(String processId);
 
+    List<ApplicationRecord> findByUsers(UserEntity userEntity);
     /* Optional<ApplicationRecord> findByModelResource(ModelResourceEntity modelResourceEntity);
 
      Optional<ApplicationRecord> findByAlgorithmServer(AlgorithmAndServerEntity algorithmAndServerEntity);
